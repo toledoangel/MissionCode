a38e75f709b78f5ecbc0f67f45569bf1c361377e
Arelis Alejandra Reyes Guerra <arelisareyes.gr@ibm.com>
2023-07-06 14:59:11 -0600

    fondos actualizados

--- test_v2.py
+++ test_v2.py
@@ -2,39 +2,91 @@ import sys
 import pygame
 import pygame_menu
 
-ANCHO = 800
-ALTO = 600
+#definicion del alto y ancho
+ANCHO = 600
+ALTO = 400
 
+#colores de fondo
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 
+#metodos que necesita pygame para iniciar
 pygame.init()
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("MissionCode")
 
-class Pantalla:
 
-    fondo = pygame.image.load("Imagenes/Mockups/menu-principal.png")
+#para cambiar de pantalla
+pantalla_actual = None
 
-    #Elementos que se repiten en las distintas pantallas
-    def __init__(self, fondo):
-        self.fondo = pygame.image.load(fondo).convert()
-        self.start = pygame.image.load('Imagenes/Animaciones/cohete.png')
-        self.boton_start = self.start.get_rect(center=(100,200))
+class Menu_Principal():
 
-
-    #Muestra la pantalla
-    def mostrar_pantalla(self):
-        pantalla.blit(self.fondo, (0, 0))
-        pygame.display.flip()
-
-
-class MenuPrincipal(Pantalla):
     def __init__(self):
-        super().__init__("Imagenes/Mockups/instrucciones.png")
-        boton_start = self.boton_start
-        fuente = pygame.font.SysFont('Arial', 32)
-        self.texto_cohete = fuente.render('Programa tu propio cohete', True, BLANCO)
+        self.fondo = pygame.image.load("Imagenes/Mockups/menufinal.png")
+        self.boton_start = pygame.image.load('Imagenes/Animaciones/cohete.png')
+
+        #self.fondo = self.fondo.convert()
+
+        #super().__init__("Imagenes/Mockups/menu-principal.png")
+        """self.boton_start = pygame_menu.widgets.Button(title='',
+                                                       image_path='Imagenes/Animaciones/cohete.png',
+                                                       image_width=100,
+                                                       image_height=100,
+                                                       onselect=self.funcion_del_boton)
+        """
+    def funcion_del_boton(self, boton_start):
+        pantalla.blit(self.boton_start, (180, 50))
+
+
+menu_principal = Menu_Principal()
+
+while True:
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            # pantalla_actual.mostrar_pantalla_principal()
+            pantalla_actual = menu_principal
+
+
+
+    pantalla.blit(menu_principal.fondo,(0, 0))
+    pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class PantallaDespliegue(Pantalla):
@@ -81,16 +133,12 @@ class PantallaEligeCohete(Pantalla):
  #   global pantalla_actual
     #pantalla_actual = pantalla_instrucciones
 
-def mostrar_pantalla_principal():
-    global pantalla_actual
-    pantalla_actual = MenuPrincipal()
-    #pantalla_principal = MenuPrincipal()
 
 
-menu = pygame_menu.Menu(height=400,
+"""menu = pygame_menu.Menu(height=400,
     theme=pygame_menu.themes.THEME_DARK,
     title='MissionCode',
-    width=600)
+    width=600)"""
 
 #instanciar las clases
 #pantalla_despliegue = PantallaDespliegue()
@@ -107,7 +155,7 @@ menu = pygame_menu.Menu(height=400,
 #menu.add.button('Pantalla de Instrucciones', mostrar_pantalla_instrucciones)
 
 
-menu.add.button('Start',mostrar_pantalla_principal)
+"""menu.add.button('Start',mostrar_pantalla_principal)
 menu.add.button('Salir', pygame_menu.events.EXIT)
 
 
@@ -139,4 +187,4 @@ while True:
             elif pantalla_actual is not None:
                 pantalla_actual.mostrar_pantalla()
 
-        pygame.display.flip()
+        pygame.display.flip()"""

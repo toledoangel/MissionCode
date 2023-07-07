33ab9f3f093c0f87be7d4bed084efc43b48928a7
Arelis Alejandra Reyes Guerra <arelisareyes.gr@ibm.com>
2023-07-06 20:11:25 -0600

    segunda y tercera pantalla

--- test_v2.py
+++ test_v2.py
@@ -21,32 +21,69 @@ class Menu_Principal():
     def __init__(self):
         self.fondo = pygame.image.load("Imagenes/Mockups/menufinal.png")
         self.boton_start = pygame.image.load('Imagenes/Animaciones/start.png')
-        self.boton_start_rect = self.boton_start.get_rect(center=(ANCHO // 2, ALTO // 1))
+        self.cohete = pygame.image.load('Imagenes/Animaciones/Ariane5pequeño.svg')
+        self.boton_start_rect = self.boton_start.get_rect(left=100, centery=ALTO // 2)
         self.fuente = pygame.font.Font(None ,32)
 
     def draw(self):
         pantalla.blit(self.fondo, (0, 0))
         pantalla.blit(self.boton_start, self.boton_start_rect)
         texto = self.fuente.render("Programa tu propio cohete", True, BLANCO)
-        rect_texto = texto.get_rect(center=(ANCHO // 2, ALTO // 2 - 100))
+        rect_texto = texto.get_rect(center=(200, 100))
+        texto_creditos = self.fuente.render("Creditos", True, BLANCO)
+        rect_texto_creditos = texto.get_rect(center=(550, 360))
+        pantalla.blit(self.cohete, (420, 140))
         pantalla.blit(texto, rect_texto)
+        pantalla.blit(texto_creditos, rect_texto_creditos)
         pygame.display.flip()
 
-class Instrucciones:
+class EligeCohete:
     def __init__(self):
         self.fondo = pygame.image.load("Imagenes/Mockups/elige-cohete.png")
         self.boton_start = pygame.image.load('Imagenes/Animaciones/start.png')
-        self.boton_start_rect = self.boton_start.get_rect(center=(ANCHO // 2, ALTO // 1))
+        self.boton_start_rect = self.boton_start.get_rect(center=(300, 320))
+        self.cohete = pygame.image.load('Imagenes/Animaciones/Ariane5pequeño.svg')
+        self.cohete_1 = pygame.image.load('Imagenes/Animaciones/Ariane5pequeño.svg')
+        self.cohete_2 = pygame.image.load('Imagenes/Animaciones/Ariane5pequeño.svg')
+        self.fuente = pygame.font.Font(None, 32)
 
     def draw(self):
         pantalla.blit(self.fondo, (0, 0))
         pantalla.blit(self.boton_start, self.boton_start_rect)
+        texto = self.fuente.render("Elige tu cohete", True, BLANCO)
+        rect_texto = texto.get_rect(center=(300, 86))
+        pantalla.blit(texto, rect_texto)
+        pantalla.blit(self.cohete, (440, 100))
+        pantalla.blit(self.cohete_1, (240, 100))
+        pantalla.blit(self.cohete_2, (340, 100))
         pygame.display.flip()
 
 
+class Instrucciones:
+
+    def __init__(self):
+        self.fondo = pygame.image.load("Imagenes/Mockups/instrucciones.png")
+        self.boton_start = pygame.image.load('Imagenes/Animaciones/start.png')
+        self.boton_start_rect = self.boton_start.get_rect(center=(430, 320))
+        self.cohete = pygame.image.load('Imagenes/Animaciones/Ariane5pequeño.svg')
+        self.fuente = pygame.font.Font(None, 32)
+
+    def draw(self):
+        pantalla.blit(self.fondo, (0, 0))
+        pantalla.blit(self.boton_start, self.boton_start_rect)
+        texto = self.fuente.render("Ordena las instrucciones para el despegue", True, BLANCO)
+        rect_texto = texto.get_rect(center=(300, 66))
+        pantalla.blit(texto, rect_texto)
+        pantalla.blit(self.cohete, (420, 140))
+        pygame.display.flip()
+
+
+
 menu_principal = Menu_Principal()
+elige_cohete = EligeCohete()
 instrucciones = Instrucciones()
-#para cambiar de pantalla
+
+#inicio
 pantalla_actual = menu_principal
 
 
@@ -59,12 +96,14 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if pantalla_actual == menu_principal and menu_principal.boton_start_rect.collidepoint(mouse_pos):
-                pantalla_actual = instrucciones
-            elif pantalla_actual == instrucciones and instrucciones.boton_start_rect.collidepoint(mouse_pos):
-                pantalla_actual = menu_principal
+                pantalla_actual = elige_cohete
+            elif pantalla_actual == elige_cohete and elige_cohete.boton_start_rect.collidepoint(mouse_pos):
+                pantalla_actual = instrucciones #cambia a la siguiente
 
     if pantalla_actual == menu_principal:
         menu_principal.draw()
+    elif pantalla_actual == elige_cohete:
+        elige_cohete.draw()
     elif pantalla_actual == instrucciones:
         instrucciones.draw()
 
